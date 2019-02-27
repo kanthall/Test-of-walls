853f08bf6bdd5926266e5138a1db13c34f9cd2a6
Paweł T <kantal.streamy@gmail.com>
2019-02-27 22:17:37 +0100

    Test build
    Testing build of settings

---
+++ Assets/BasicMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal")); 
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position = transform.position + horizontal * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+
+    public float speed;             
+    private Rigidbody2D rb2d;
+    [SerializeField] Animator animator;
+
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+       
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);      
+        rb2d.AddForce(movement * speed * Time.deltaTime);
+    }
+}
