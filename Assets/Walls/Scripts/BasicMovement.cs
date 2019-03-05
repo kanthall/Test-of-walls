bf23b909dac9995c895c4e19e1e912f93cace10d
Paweł T <kantal.streamy@gmail.com>
2019-03-05 22:28:22 +0100

    Test nr 3
    Test for raycast2d

--- Assets/Walls/Scripts/BasicMovement.cs
+++ Assets/Walls/Scripts/BasicMovement.cs
@@ -1,47 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private Animator animator;
 
-    [SerializeField] float speed;
-    public Animator animator;
-    [SerializeField] GameObject targetPoint;
+    private float raycastDistance = 3;
 
-    void Start()
+    private void Update()
     {
-        
-    }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-    // Update is called once per frame
-    void Update()
-    {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-            
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), (Input.GetAxis("Vertical")), 0f);
-        transform.position = transform.position + move * Time.deltaTime * speed;
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
 
+        Vector3 move = new Vector3(horizontal, vertical, 0f);
+        transform.position = transform.position + move * Time.deltaTime * moveSpeed;
 
-        Vector3 raycastDir = new Vector3(2f, 0f, 0f);
-        Vector3 posRay = new Vector3(2f, 0f, 0f);
+        if (horizontal == 0 && vertical == 0)
+            return;
 
-        Debug.DrawRay(transform.position, raycastDir, Color.red);
+        Vector3 raycastStart = transform.position;
+        Vector3 raycastDirection = Vector3.zero;
 
-        RaycastHit2D hit = Physics2D.Raycast(raycastDir, Vector3.zero);
-
-        //If something was hit, the RaycastHit2D.collider will not be null.
-        if (hit.collider != null)
+        if (horizontal != 0)
         {
-            Debug.Log("Hit");
-        } 
-        else if (hit.collider.name == "Wall")
-            {
-            Debug.Log("ściana");
+            if (horizontal > 0)
+                raycastDirection = transform.right * 1;
+            else
+                raycastDirection = transform.right * -1;
         }
 
+        if (vertical != 0)
+        {
+            if (vertical > 0)
+                raycastDirection = transform.up * 1;
+            else
+                raycastDirection = transform.up * -1;
+        }
 
+        RaycastHit2D hit = Physics2D.Raycast(raycastStart, raycastDirection, raycastDistance);
+        Debug.DrawRay(raycastStart, raycastDirection * raycastDistance, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Obstacle")
+            {
+                Debug.Log("przeszkoda");
+            }
+        }
     }
 }
-
